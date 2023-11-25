729ea3c32bc0924341230a091a328acb2776c37b
Punff <marioantunovic13@gmail.com>
2023-11-25 19:11:09 +0100

    Map size calculation and drawing / window shenanignas

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
-#include "main.hpp"
+#include <iostream>
+#include "map.hpp"
+#include <raylib.h>
+#include <vector>
+using namespace std;
 
 int main() {
-    InitWindow(800, 600, "raylib");
+    InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "raylib");
+    ToggleFullscreen();
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         // Updates
+        Map map;
+        map.set_size(GetScreenWidth());
+        map.set_poz(GetScreenWidth() / 2, GetScreenHeight() / 2);
         
 
         // Draw
         BeginDrawing();
+        
+        ClearBackground(BLACK);
+
+        DrawRectangleLines(map.x - map.size / 2, map.y - map.size / 2, map.size, map.size, RAYWHITE);
         
         EndDrawing();
     }
