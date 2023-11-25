8c513599e676dcce9cc66b4055dbfdfbae1dd30d
Punff <marioantunovic13@gmail.com>
2023-11-25 15:17:33 +0100

    clean slate

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <raylib.h>
+#include <vector>
+using namespace std;
+
+int main() {
+    InitWindow(800, 600, "raylib");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        // Updates
+        
+
+        // Draw
+        BeginDrawing();
+        
+        EndDrawing();
+    }
+
+    return 0;
+}
