0b2bbdb554187a81b4e43b97c9d1398a14989f31
Gr0undZero <teo.knezovic@gmail.com>
2023-11-28 13:06:37 +0100

    cleaned template

---
+++ include/tiles.hpp
@@ -0,0 +1,14 @@
+#ifndef TILES_H
+#define TILES_H
+
+class baseTile{
+    public:
+    baseTile();
+    virtual void draw_tile(int xPos, int yPos, int size);
+};
+
+class wireframeTile : public baseTile{
+    void draw_tile(int xPos, int yPos, int size);
+};
+
+#endif
--- src/baseTile.cpp
+++
@@ -1,5 +0,0 @@
-#include "baseTile.hpp"
-
-baseTile::baseTile(){
-    
-}
