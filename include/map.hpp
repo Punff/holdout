5401722de5b0b8f94af30b95c7e1922cc3526cea
Gr0undZero <teo.knezovic@gmail.com>
2023-12-02 21:06:13 +0100

    tiles rolled back, map can now give tile coords

--- include/map.hpp
+++ include/map.hpp
@@ -14,9 +14,13 @@ class Map{
     public:
     vector<vector<baseTile*>> grid;
     Map();
+    //Map(string filename);
     void draw_map();
     void set_size(int size);
     void set_pos(int xPos, int yPos);
+    int get_tile_xPos(int column);
+    int get_tile_yPos(int row);
+    int get_tile_size();
 };
 
 #endif
--- include/tiles.hpp
+++ include/tiles.hpp
@@ -2,19 +2,13 @@
 #define TILES_H
 
 class baseTile{
-    protected:
-    int xPos, yPos, size;
     public:
-    baseTile(int xPos, int yPos, int size);
-    int get_xPos();
-    int get_yPos();
-    int get_size();
-    virtual void draw_tile();
+    baseTile();
+    virtual void draw_tile(int xPos, int yPos, int size);
 };
 
 class wireframeTile : public baseTile {
-    using baseTile::baseTile;
-    void draw_tile();
+    void draw_tile(int xPos, int yPos, int size);
 };
 
 #endif
