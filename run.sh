3f5bc471375d0f1f365d37735dcc69e3b6974495
Teo Knezovic <teo.knezovic@gmail.com>
2023-12-04 12:09:37 +0100

    rebuilding now removes deleted assets

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-cmake -S . -B build && rm -rv build/assets && cp -r assets build && pushd build; make && ./raylib_game; popd
+rm -r build/assets;
+cmake -S . -B build && cp -r assets build && pushd build; make && ./raylib_game; popd
