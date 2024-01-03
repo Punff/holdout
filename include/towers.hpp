afb73cb59605f3fe57cb7543d7aa43a82e99e095
punff <marioantunovic13@gmail.com>
2024-01-03 14:24:22 +0100

    converted the rest of the shit code to the superior Vec2

--- include/towers.hpp
+++ include/towers.hpp
@@ -1,28 +1,26 @@
 #ifndef TOWERS_H
 #define TOWERS_H
 #include "tiles.hpp"
+#include "Vec2.hpp"
 
 class baseTower {
 	protected:
-	int xPos;
-	int yPos;
+    Vec2 position;
 	int HP;
 	int damage;
 	float range;
 	int cost;
 	
 	public:
-	// finish constructor
-	baseTower();
-    virtual void set_position(int xPos, int yPos);
+	baseTower(float x, float y);
     virtual void draw_tower(int size);
     virtual void draw_range(float range, int size);
 };
 
 class basicTower : public baseTower {
 	public:
-	void set_position(int xPos, int yPos);
-	void draw_tower(int size);
-	void draw_range(float range, int size);
+    basicTower(float x, float y);
+	void draw_tower(int size) override;
+	void draw_range(float range, int size) override;
 };
 #endif
