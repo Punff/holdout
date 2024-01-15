5abee53e9eb95e85025794ccdeaa374341ae4968
Teo <teo.knezovic@gmail.com>
2024-01-15 19:50:37 +0100

    renamed game in build folder

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -r build/assets;
-cmake -S . -B build && cp -r assets build && pushd build; make && ./raylib_game; popd
+cmake -S . -B build && cp -r assets build && pushd build; make && ./holdout; popd
